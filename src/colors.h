13112b0aba38a827c240921d6c978142d1cbf977
DragonSlayer <varattleff@Viktors-MacBook-Air.local>
2024-09-13 00:14:34 +0200

    update colors

---
+++ src/colors.h
@@ -0,0 +1,22 @@
+#ifndef COLORS_H
+#define COLORS_H
+
+
+#define WHITE   "\033[37m"
+#define BLACK   "\033[30m"
+#define GREEN   "\033[32m"
+#define BLUE    "\033[34m"
+#define YELLOW  "\033[33m"
+#define RED     "\033[31m"
+
+#define BG_WHITE   "\033[47m"
+#define BG_BLACK   "\033[40m"
+#define BG_GREEN   "\033[42m"
+#define BG_BLUE    "\033[44m"
+#define BG_YELLOW  "\033[43m"
+#define BG_RED     "\033[41m"
+
+#define RESET   "\033[0m"
+#define RESET_COLOR RESET
+
+#endif
